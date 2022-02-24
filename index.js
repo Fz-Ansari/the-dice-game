81f7b58312dcc2bb3faf42b3b2ce395326339d47
faizan ansari <92996125+Fz-Ansari@users.noreply.github.com>
2022-02-24 10:14:01 +0530

    index.js
    done

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1 );
 var randomNumber2 = Math.floor((Math.random() * 6) + 1 );
 // console.log(randomNumber1);
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src","dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","dice"+randomNumber2+".png");
 
 if(randomNumber1>randomNumber2){
 	document.querySelector("h1").innerHTML = "Player1 win";
